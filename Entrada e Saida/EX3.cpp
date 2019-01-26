26d862e9f60329f6d2a324dd14f9716975c7923c
davicarv13 <davicarv13@gmail.com>
2019-01-26 07:51:03 -0200

    Adiciona EX3.c e README.md

---
+++ Entrada e Saida/EX3.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	char frase1[20], frase2[20];
+	cout << "Informe um frase:";
+	cin >> frase1;
+	cout << "Informe outra frase:";
+	cin >> frase2;	
+	cout << "Frase concatenadas:" << frase1 << frase2;
+}
--- Entrada e Saida/README.md
+++ Entrada e Saida/README.md
@@ -1,3 +1,5 @@
 1) Hello World
 
 2) Área do retângulo
+
+3) Imprime frases concatenadas na tela
